e6b04fc553976671ac31be7f6db585c3b3b1f8d6
“Marcelchia” <marcel.chiayl@gmail.com>
2020-04-29 22:22:33 +0800

    draft

--- views/Register.jsx
+++ views/Register.jsx
@@ -1,57 +1,51 @@
-var React = require("react");
 
+var React = require('react');
 class Register extends React.Component {
   render() {
-    return (<html>
-        <head>
-           <link rel="stylesheet" type="text/css" href="/style.css"/>
+    return (
+      <html>
+    <head>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossOrigin="anonymous"/>
+          <link rel="stylesheet" type="text/css" href="/style.css"/>
+          <title>My Library</title>
+          <meta charSet="utf-8" />
+      </head>
 
-            <title>MooTrek</title>
-            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossOrigin="anonymous"/>
-            <link rel="stylesheet" type="text/css" href="/style.css"/>
-            <title>MooTrek</title>
-            <meta charSet="utf-8" />
+
+        <body>
 
 
-        </head>
+<div className="card mb-3" >
+  <div className="card-body" >
+    <h1 className="card-title">Register Here</h1>
+    <p className="card-text">
+        <div >
+            <form action="/register" method="POST">
+                <p>Name <input name="name" required/>
+                </p>
+                <p>Password <input type="password" name="password"/>
+                </p>
+                <p>
+                    <input type="submit" value="submit" className="btn btn-info"/>
+                </p>
+            </form>
+        </div>
 
-<body>
-    <div className="card mb-3" >
-            <div className="card-body" >
-                    <h1 className="card-title">Login</h1>
-                    <p className="card-text">
-                                <div >
-                                        <form action="/login" method="POST">
-                                             <p style>
-                                                    Name <input name="name" required/>
-                                            </p>
+          <div>
+              <p>Registered? Click<a href="/login"> here</a> to login.</p>
+        </div>
 
-                                            <p style>
-                                                Password <input type="password" name="password"/>
-                                            </p>
 
-                                            <p>
-                                                <input type="submit" value="submit" className="btn btn-info"/>
-                                            </p>
-                                        </form>
-                                </div>
 
-                            <div>
-                                    <p style={{width: "30%", textAlign: "center", margin: "50px auto 5px auto"}}>Not registered yet? Click<a href="/register"> here</a> to register.</p>
-                            </div>
-                    </p>
-                            <p className="card-text"></p>
-            </div>
+    </p>
+    <p className="card-text"></p>
+  </div>
 </div>
 
         </body>
- </html>
-            )
+      </html>
+    );
+  }
+}
 
-        }
-
-    };
-
-
-
-    module.exports = Register;
+module.exports = Register;
