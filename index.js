32aac30b8a8abead1866ff00c4e1ed66c1282db5
“Marcelchia” <marcel.chiayl@gmail.com>
2020-04-29 22:21:56 +0800

    touch up

--- index.js
+++ index.js
@@ -55,40 +55,40 @@ process.on('SIGINT', onClose);
 
 
 
-const pg = require('pg');
-const url = require('url');
+// const pg = require('pg');
+// const url = require('url');
 
-var configs;
+// var configs;
 
-if( process.env.DATABASE_URL ){
+// if( process.env.DATABASE_URL ){
 
-  const params = url.parse(process.env.DATABASE_URL);
-  const auth = params.auth.split(':');
+//   const params = url.parse(process.env.DATABASE_URL);
+//   const auth = params.auth.split(':');
 
-  configs = {
-    user: auth[0],
-    password: auth[1],
-    host: params.hostname,
-    port: params.port,
-    database: params.pathname.split('/')[1],
-    ssl: true
-  };
+//   configs = {
+//     user: auth[0],
+//     password: auth[1],
+//     host: params.hostname,
+//     port: params.port,
+//     database: params.pathname.split('/')[1],
+//     ssl: true
+//   };
 
-}else{
-  configs = {
-    user: 'apple',
-    host: '127.0.0.1',
-    database: 'mootrek',
-    port: 5432
-  };
-}
+// }else{
+//   configs = {
+//     user: 'apple',
+//     host: '127.0.0.1',
+//     database: 'mootrek',
+//     port: 5432
+//   };
+// }
 
 
-const pool = new pg.Pool(configs);
+// const pool = new pg.Pool(configs);
 
-pool.on('error', function (err) {
-  console.log('idle client error', err.message, err.stack);
-});
+// pool.on('error', function (err) {
+//   console.log('idle client error', err.message, err.stack);
+// });
 
 
 // ************************************************
