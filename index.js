f7a77ddc791f2f8e6779f479011357b92ff71bb7
“Marcelchia” <marcel.chiayl@gmail.com>
2020-05-04 23:17:26 +0800

    draft

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ app.post('/register', (request, response) => {
       let user_id = result.rows[0].id;
       let hashedCookie = sha256(SALT+user_id);
       response.cookie('name', request.body.name);
-      response.cookie('loggedIn', hashedCookie);
+      response.cookie('loggedOut', hashedCookie);
       response.cookie('userId', user_id);
       response.redirect('/login');
     }
